fd93ff80ef5f65ab712ae53a90b55ff572683f95
Yasharth Rai <yasharth.rai@example.com>
2026-04-22 17:57:15 +0530

    Fix build configuration for GitHub Actions

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'export',
-  basePath: '/My-Next-Portfolio',
-  assetPrefix: '/My-Next-Portfolio/',
 }
 
 module.exports = nextConfig
